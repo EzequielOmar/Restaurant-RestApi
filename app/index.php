fc773216e98f89a92adbf31f741101d160b97d74
EzequielOmar <ezequiel.omar.vazquez@gmail.com>
2021-05-23 18:45:48 -0300

    modificacion simple

--- app/index.php
+++ app/index.php
@@ -5,7 +5,6 @@ error_reporting(-1);
 ini_set('display_errors', 1);
 
 require __DIR__ . '/../vendor/autoload.php';
-require_once './controladores/loguerApi.php';
 require_once './controladores/staffApi.php';
 require_once './controladores/clienteApi.php';
 require_once './controladores/productoApi.php';
